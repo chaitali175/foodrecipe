7897b6afb86cd51fd5f276c12763e0dc90874561
Chaitali Bochare <chaitalibochare2283@gmail.com>
2020-07-17 22:16:15 +0530

    Updates

---
+++ precache-manifest.3a9331f16bf7fc335e44044591793c28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3170eb1d26fac1f9b200e4e83379fefb",
+    "url": "/foodrecipe/index.html"
+  },
+  {
+    "revision": "bab34425473168e8ce1f",
+    "url": "/foodrecipe/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "25226fa718c7f6631653",
+    "url": "/foodrecipe/static/css/main.8d8eec3c.chunk.css"
+  },
+  {
+    "revision": "bab34425473168e8ce1f",
+    "url": "/foodrecipe/static/js/2.8f5970bd.chunk.js"
+  },
+  {
+    "revision": "9e81e617707513432f3da640fbf3bd82",
+    "url": "/foodrecipe/static/js/2.8f5970bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25226fa718c7f6631653",
+    "url": "/foodrecipe/static/js/main.77dde369.chunk.js"
+  },
+  {
+    "revision": "1d5b25a378e6e1d6f6de",
+    "url": "/foodrecipe/static/js/runtime-main.51e31051.js"
+  },
+  {
+    "revision": "880f228b991fdc18f4e6930f009f81c2",
+    "url": "/foodrecipe/static/media/bg1.880f228b.jpg"
+  }
+]);
